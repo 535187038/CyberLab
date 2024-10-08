d19617ef81d714e8cf6fd747264672d2000cda46
Megan <49613350+535187038@users.noreply.github.com>
2024-10-08 17:49:24 +0800

    Update test.py

--- test1/test.py
+++ test1/test.py
@@ -1,1 +1,18 @@
+import functools
+import random
+import time
+import traceback
+import unittest
 
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from cyberium.accessory import AccessoryApi
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support.wait import WebDriverWait
+from test_cases.DesiredCapsParser import DesiredCapsParser
+
+def test():
+  num1 = 1
+  num2 = 2
+  sum = num1 + num2
+  return sum
