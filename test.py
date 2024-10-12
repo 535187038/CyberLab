4afcb714d12f884f2a4cf8b06f747257ceff5451
Megan <49613350+535187038@users.noreply.github.com>
2024-10-12 15:00:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,105 +1,48 @@
-import functools
-import random
-import time
-import traceback
-import unittest
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : CyberLab
+import pytest
 from appium import webdriver
+from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
-from cyberium.accessory import AccessoryApi
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from test_cases.DesiredCapsParser import DesiredCapsParser
 
 
-def case_exception():
-    def wrapper(func):
-        @functools.wraps(func)
-        def wrapper_(self, *args):
-            try:
-                func(self, *args)
-            except Exception as e:
-                if self.not_removed:
-                    self.api.remove_card(self.card)
-                traceback.print_exc()
-                self.driver.close_app()
-                self.driver.launch_app()
-                if not self.printable:
-                    self.wait.until(lambda x: x.find_element(
-                        AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Cancel")')).click()
-                raise e
-
-        return wrapper_
-
-    return wrapper
-
-
-class TransactionTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.desired_caps = DesiredCapsParser().get_desired_caps()
-        cls.url = DesiredCapsParser().get_url()
-        cls.driver = webdriver.Remote(cls.url, cls.desired_caps)
-        cls.token = cls.desired_caps['token']
-        cls.device_id = cls.desired_caps['deviceId']
-        cls.api = AccessoryApi(token=cls.token, device_id=cls.device_id)
-        cls.wait = WebDriverWait(cls.driver, 10)
-        cls.api.inject_dukpt_key(1, 'FFFF5B0999000000', ('0000000000000000', '0000000000000000'),
-                                 ('0000000000000000', '0000000000000000'))
-        cls.not_removed = False
-        # connect to BP60A
+# HINT: fixtures below could be extracted into conftest.py
+# HINT: and shared across all tests in the suite
+@pytest.fixture(scope="class")
+def driver() -> webdriver.Remote:
+    """Fixture to set up and teardown the appium driver."""
+    desired_caps = DesiredCapsParser().get_desired_caps()
+    options = UiAutomator2Options().load_capabilities(desired_caps)
+    options.no_reset = False
+    url = DesiredCapsParser().get_url()
+    print("url----",url)
+    driver = None
+    if not (options is None) and not (url is None):
         try:
-            cls.wait.until(lambda x: x.find_element(
-                AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Cancel")')).click()
-            cls.printable = False
-        except WebDriverException:
-            cls.printable = True
+            driver = webdriver.Remote(url, options=options)
+            driver.implicitly_wait(3)
+            yield driver
+        finally:
+            if driver:
+                driver.quit()
 
-    def setUp(self) -> None:
-        time.sleep(2)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
+class TestExample:
 
-    @case_exception()
-    def test1_credit_sale_swipe(self):
+    def test_app_is_open(self, driver):
+        """ Check if the CyberLab Sample is on the main page"""
+        # HINT: use resource id to locate the element
+        target_resource_id = "com.pax.us.pay.std.cyberlab:id/et_amount_prompt"
+        target_text = "Transaction Amount ($)"
+        text = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.ID, target_resource_id))
+        ).text
 
-        self.wait.until(lambda x: x.find_element(
-            AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().textContains("CREDIT")')).click()
-        # Click Credit Sale
-        self.wait.until(lambda x: x.find_element(
-            AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().textContains("SALE")')).click()
-        # Wait For Amount Menu
-        self.wait.until(lambda x: x.find_element(
-            AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().textContains("Please Enter Amount")'))
-        # Input Transaction Amount
-        self.wait.until(lambda x: x.find_element(
-            AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.EditText")')
-                        ).send_keys('200')
-        # Confirm
-        self.wait.until(lambda x: x.find_element(
-            AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("CONFIRM")')).click()
+        assert text == target_text
 
-        # Wait For Account Menu
-        self.wait.until(lambda x: x.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
-                                                 'new UiSelector().textContains("Please Enter Account")'))
-        start_time = int(round(time.time() * 1000))
-        # Input Card Account Number
-        result, msg = self.api.swipe_card('4012000033330026=3012')
-        self.assertEqual(result, True, msg)
-
-        # Signature
-        self.wait.until(lambda x: x.find_element(
-            AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().textContains("PLS Sign Your Name")'))
-        self.driver.swipe(100, 500, 100, 600, 500)
-        time.sleep(0.5)
-        self.wait.until(lambda x: x.find_element(
-            AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().resourceId("com.pax.us.pay.std.cyberlab:id/confirm_btn")')
-                        ).click()
-        # Print receipt
-        if self.printable:
-            self.wait.until(lambda x: x.find_element(
-                AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("OK")')).click()
-        self.get_receipt_data(start_time)
+    def test_simple_case(self, driver):
+        pass
